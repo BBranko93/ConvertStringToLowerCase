8e55e5830c861c16f04fbf97437674c0c2e16ceb
BBranko93 <bane_b80@outlook.com>
2021-02-27 20:31:29 -0700

    Add project files.

---
+++ ConvertStringToLowerCase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConvertStringToLowerCase
+{
+    class Program
+    {
+        //Write a C# program to convert a given string into lowercase.
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a word in all capitals : ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(word.ToLower());
+        }
+    }
+}
